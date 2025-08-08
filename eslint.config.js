d2c33be084d3d8d79a2d76a489a01977fd70822f
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-08 14:59:18 +0200

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+
+export default defineConfig([
+    includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+    globalIgnores([".yarn"]),
+    {
+        files: ["**/*.{js,jsx}"],
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs["recommended-latest"],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                ecmaFeatures: { jsx: true },
+                sourceType: "module",
+            },
+        },
+        rules: {
+            "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+        },
+    },
+]);
