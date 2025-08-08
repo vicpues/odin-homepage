c8e755a32a19eca9b207e6ec24e17387a3b2e95a
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-08 16:59:01 +0200

    feat: Do basic hero figure styling

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,3 +1,4 @@
+import "./Hero.css";
 import CrabHeroImage from "./assets/images/crab-hero.jpg";
 
 export default function Hero() {
@@ -9,6 +10,13 @@ export default function Hero() {
           <h1>Vic Pues</h1>
         </figcaption>
       </figure>
+      <h2>About me</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ratione at
+        cupiditate aut quisquam nam. Libero illo dignissimos, consequatur
+        laborum quasi sed eius voluptates, veritatis odit deserunt aperiam
+        distinctio velit.
+      </p>
     </section>
   );
 }
