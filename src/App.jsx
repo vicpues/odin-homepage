5bc6f364246674dd1b69f810a6444f93c0c70132
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-09 15:18:46 +0200

    feat: Add other sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Hero from "./components/hero/Hero";
+import MyWork from "./components/my-work/MyWork";
+import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <main>
-      <Hero />
-    </main>
+    <>
+      <main>
+        <Hero />
+        <MyWork />
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/my-work/MyWork.jsx
@@ -0,0 +1,5 @@
+import "./MyWork.css";
+
+export default function MyWork() {
+  return <section id="my-work"></section>;
+}
