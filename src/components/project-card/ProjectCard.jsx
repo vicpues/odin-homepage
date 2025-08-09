998aa78fe06fa426388240179049087301d80136
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-09 17:19:10 +0200

    feat: Add project card links

--- src/components/project-card/ProjectCard.jsx
+++ src/components/project-card/ProjectCard.jsx
@@ -1,4 +1,6 @@
 import "./ProjectCard.css";
+import GithubIcon from "../../assets/icons/github.svg?react";
+import OpenIcon from "../../assets/icons/open-in-new.svg?react";
 
 export default function ProjectCard({
   name,
@@ -13,8 +15,12 @@ export default function ProjectCard({
         <img src="" alt="" />
         <div className="name-links-container">
           <h3 className="project-name">{name}</h3>
-          <a className="project-link" href={sourceLink}></a>
-          <a className="project-link" href={liveLink}></a>
+          <a className="project-link" href={sourceLink}>
+            <GithubIcon className="project-icon github" />
+          </a>
+          <a className="project-link" href={liveLink}>
+            <OpenIcon className="project-icon open" />
+          </a>
         </div>
         <p className="project-description">{description}</p>
       </article>
