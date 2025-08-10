23447ffc41bfccf634ec63487921fe97d15fe499
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-10 19:34:33 +0200

    feat: Make hero section responsive on tablet

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,29 +6,27 @@ import ContactLinks from "../contact-links/ContactLinks";
 export default function Hero() {
   return (
     <section id="hero">
-      <figure>
-        <ResponsiveImage
-          src={CrabHeroImage}
-          alt=""
-          className="hero-image"
-          loading="eager"
-        />
-        <figcaption>
-          <h1>
-            Vic
-            <br />
-            Pues
-          </h1>
-        </figcaption>
-      </figure>
-      <h2>About me</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ratione at
-        cupiditate aut quisquam nam. Libero illo dignissimos, consequatur
-        laborum quasi sed eius voluptates, veritatis odit deserunt aperiam
-        distinctio velit.
-      </p>
-      <ContactLinks />
+      <div className="hero-container">
+        <figure>
+          <ResponsiveImage
+            src={CrabHeroImage}
+            alt=""
+            className="hero-image"
+            loading="eager"
+          />
+          <figcaption>
+            <h1>Vic Pues</h1>
+          </figcaption>
+        </figure>
+        <h2>About me</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ratione
+          at cupiditate aut quisquam nam. Libero illo dignissimos, consequatur
+          laborum quasi sed eius voluptates, veritatis odit deserunt aperiam
+          distinctio velit.
+        </p>
+        <ContactLinks />
+      </div>
     </section>
   );
 }
