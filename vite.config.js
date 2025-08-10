95f98a54d2169f65551387cd4882c22fce92969c
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-10 14:59:17 +0200

    build: Add responsive image plugin to vite pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { setupPlugins } from "@responsive-image/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [
+        react(),
+        svgr(),
+        setupPlugins({
+            include: /^[^?]+\.jpg\?.*responsive.*$/,
+            styles: "inline",
+        }),
+    ],
 });
