423dde05779239263df01fe538723f3f4e0bd96f
Vic Pues <141432283+vicpues@users.noreply.github.com>
2025-08-12 21:16:46 +0200

    feat: Limit footer width on huge screens

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,24 +6,33 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer id="footer">
-      <div className="contact">
-        <h2>Contact me</h2>
-        <p className="message">
-          Please get in touch if you think our work could be mutually beneficial
-        </p>
-        <p>
-          1234 Random Road
-          <br />
-          Random Town, California 12345
-        </p>
-        <address>
-          <a href="tel:+NOT-A-REAL-PHONE">555-555-55555</a>
-          <br />
-          <a href="mailto:NOT-A-REAL-ADDRESS">vicpues.is.not.real@gmail.com</a>
-        </address>
-        <ContactLinks />
+      <div className="footer-container">
+        <div className="contact">
+          <h2>Contact me</h2>
+          <p className="message">
+            Please get in touch if you think our work could be mutually
+            beneficial
+          </p>
+          <p>
+            1234 Random Road
+            <br />
+            Random Town, California 12345
+          </p>
+          <address>
+            <a href="tel:+NOT-A-REAL-PHONE">555-555-55555</a>
+            <br />
+            <a href="mailto:NOT-A-REAL-ADDRESS">
+              vicpues.is.not.real@gmail.com
+            </a>
+          </address>
+          <ContactLinks />
+        </div>
+        <ResponsiveImage
+          src={CrabFooterImage}
+          alt=""
+          className="footer-image"
+        />
       </div>
-      <ResponsiveImage src={CrabFooterImage} alt="" className="footer-image" />
     </footer>
   );
 }
